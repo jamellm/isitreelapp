7d642dc2f9252bf6700d0fb6089eed8ce43fe3fe
jamellm <jamell.mobley@gmail.com>
2026-04-29 07:17:22 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
