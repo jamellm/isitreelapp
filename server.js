fb6bfb04b467a341df2bf6df2cac84f6f0538a17
jamellm <jamell.mobley@gmail.com>
2026-04-29 07:34:21 -0400

    Refactor server.js to use CommonJS syntax

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from dist
-app.use(express.static(join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Handle client-side routing
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`IsItReel running on port ${PORT}`);
+  console.log('IsItReel running on port ' + PORT);
 });
